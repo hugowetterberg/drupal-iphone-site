06dc73b2585304c0c7ea75221d35d41ffc2fa23c
Hugo Wetterberg <hugo.wetterberg@goodold.se>
2008-12-08 11:35:31 +0100

    flag 6.x-1.0-beta6

---
+++ sites/all/modules/flag/theme/flag.js
@@ -0,0 +1,101 @@
+// $Id: flag.js,v 1.1.2.4 2008/10/10 06:52:31 quicksketch Exp $
+
+/**
+ * Terminology:
+ *
+ *   "Link" means "Everything which is in flag.tpl.php" --and this may contain
+ *   much more than the <A> element. On the other hand, when we speak
+ *   specifically of the <A> element, we say "element" or "the <A> element".
+ */
+
+Drupal.flagLink = function(context) {
+  /**
+   * Helper function. Updates a link's HTML with a new one.
+   *
+   * @param element
+   *   The <A> element.
+   * @return
+   *   The new <A> elemenet.
+   */
+  function updateLink(element, newHtml) {
+    var $newLink = $(newHtml);
+
+    // Initially hide the message so we can fade it in.
+    $('.flag-message', $newLink).css('display', 'none');
+
+    // Reattach the behavior to the new <A> element. This element
+    // is either whithin the wrapper or it is the outer element itself.
+    var $nucleus = $newLink.is('a') ? $newLink : $('a.flag', $newLink);
+    $nucleus.addClass('flag-processed').click(flagClick);
+
+    // Find the wrapper of the old link.
+    var $wrapper = $(element).parents('.flag-wrapper:first');
+    if ($wrapper.length == 0) {
+      // If no ancestor wrapper was found, or if the 'flag-wrapper' class is
+      // attached to the <a> element itself, then take the element itself.
+      $wrapper = $(element);
+    }
+    // Replace the old link with the new one.
+    $wrapper.after($newLink).remove();
+
+    // So it won't crash on Drupal 5.
+    if (Drupal.attachBehaviors) {
+      Drupal.attachBehaviors($newLink.get(0));
+    }
+
+    $('.flag-message', $newLink).fadeIn();
+  }
+  
+  /**
+   * A click handler that is attached to all <A class="flag"> elements.
+   */
+  function flagClick() {
+    // 'this' won't point to the element when it's inside the ajax closures,
+    // so we reference it using a variable.
+    var element = this;
+
+    // While waiting for a server response, the wrapper will have a
+    // 'flag-waiting' class. Themers are thus able to style the link
+    // differently, e.g., by displaying a throbber.
+    var $wrapper = $(element).parents('.flag-wrapper');
+    if ($wrapper.is('.flag-waiting')) {
+      // Guard against double-clicks.
+      return false;
+    }
+    $wrapper.addClass('flag-waiting');
+
+    // Hide any other active messages.
+    $('span.flag-message:visible').fadeOut();
+
+    // Send POST request
+    $.ajax({
+      type: 'POST',
+      url: element.href,
+      data: { js: true },
+      dataType: 'json',
+      success: function (data) {
+        if (data.status) {
+          // Success.
+          updateLink(element, data.newLink);
+        }
+        else {
+          // Failure.
+          alert(data.errorMessage);
+          $wrapper.removeClass('flag-waiting');
+        }
+      },
+      error: function (xmlhttp) {
+        alert('An HTTP error '+ xmlhttp.status +' occurred.\n'+ element.href);
+      }
+    });
+    return false;
+  }
+
+  $('a.flag:not(.flag-processed)').addClass('flag-processed').click(flagClick);
+};
+
+
+Drupal.behaviors.flagLink = function(context) {
+  // On load, bind the click behavior for all links on the page.
+  Drupal.flagLink(context);
+};
