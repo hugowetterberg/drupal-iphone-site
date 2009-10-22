9e178fde2ff0d00781951c71e8bdda1416d6230a
Pelle Wessman <pelle@kodfabrik.se>
2009-10-22 10:06:16 +0200

    Updated Pathauto to 6.x-1.2

--- sites/all/modules/pathauto/pathauto.js
+++ sites/all/modules/pathauto/pathauto.js
@@ -1,4 +1,4 @@
-// $Id: pathauto.js,v 1.4 2008/03/06 02:56:05 greggles Exp $
+// $Id: pathauto.js,v 1.4.2.1 2009/04/18 17:10:10 freso Exp $
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
     if ($("#edit-pathauto-perform-alias").size() && $("#edit-pathauto-perform-alias").attr("checked")) {
@@ -10,13 +10,13 @@ if (Drupal.jsEnabled) {
       if ($("#edit-pathauto-perform-alias").attr("checked")) {
         // Auto-alias checked; disable input.
         $("#edit-path").attr("disabled","disabled");
-        $("#edit-path-wrapper > div[@class=description]").slideUp('fast');
+        $("#edit-path-wrapper > div[class=description]").slideUp('fast');
       }
       else {
         // Auto-alias unchecked; enable input.
         $("#edit-path").removeAttr("disabled");
         $("#edit-path")[0].focus();
-        $("#edit-path-wrapper > div[@class=description]").slideDown('fast');
+        $("#edit-path-wrapper > div[class=description]").slideDown('fast');
       }
     });
   });
